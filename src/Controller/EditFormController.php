4ff0cf07e943f9cd377dc48781f93a9e8961ce30
TheAwesomeDelta <66464759+TheAwesomeDelta@users.noreply.github.com>
2022-07-21 11:37:42 +0200

    Form is functionnal
    - Added EditFormType
    - Modified edit.html.twig (formfields) and EditFormController (displayInfo)

--- src/Controller/EditFormController.php
+++ src/Controller/EditFormController.php
@@ -1,41 +1,60 @@
 <?php
-    namespace App\Controller;
 
-    use App\Entity\Pays;
-    use Doctrine\Persistence\ManagerRegistry;
-    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-    use Symfony\Component\Routing\Annotation\Route;
+namespace App\Controller;
 
-    class EditFormController extends AbstractController {
+use App\Entity\Pays;
+use App\Form\EditFormPaysType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
-        /**
-        * @Route(
-        *        "/edit_pays/{id}",
-        *        name="edit_pays",
-        *        defaults={"id": "1"}
-        * )
-        */
-        
-        public function displayInfo(ManagerRegistry $doctrine, $id) {
-            $alert ='';
-            $result = [];
+class EditFormController extends AbstractController
+{
+
+    /**
+     * @Route(
+     *        "/edit_pays/{id}",
+     *        name="edit_pays",
+     *        defaults={"id": "1"}
+     * )
+     */
+
+    public function displayInfo(ManagerRegistry $doctrine, $id, Request $request)
+    {
+        $alert = '';
+        $result = [];
+        $info = '';
 
         //Verifying if the id exists
-        if (!($id >=1 && $id <= 237)) {
-            
+        if (!($id >= 1 && $id <= 237)) {
+
             //If the id does not exist, the edit page will show the first country in the list (here, France),
             //and return an error message
             $id = 1;
             $alert = "Erreur: l'id est innexistant, affichage de la valeur par défaut";
         }
-            try {
+        try {
 
-                $doc_db = $doctrine->getRepository(Pays::class);
+            $paysRep = $doctrine->getRepository(Pays::class);
 
-                $result = $doc_db->find($id);
-
-            } catch (\Exception $e) {
+            $result = $paysRep->find($id);
+        } catch (\Exception $e) {
             $alert = $e->getMessage();
-        } return $this->render('edit/edit.html.twig', ['result' => $result, 'errors' => $alert]);
+        }
+
+            $doc_man = $doctrine->getManager();
+
+            $form = $this->createForm(EditFormPaysType::class, $result);
+
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $doc_man->persist($result);
+                $doc_man->flush();
+                $info = 'Pays mis à jour';
+            }
+
+        return $this->renderForm('edit/edit.html.twig', ['form' => $form, 'errors' => $alert, 'info' => $info, "result"=>$result, "formSubmitted"=>$form->isSubmitted()]);
     }
 }
